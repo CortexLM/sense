5c90396121b34a194fc3117daad7383c1f06e5c8
Echo <echo@cortex.foundation>
2024-01-05 12:20:10 +0000

    change default host & port

--- run.py
+++ run.py
@@ -8,13 +8,13 @@ from utils.fastapi import DaemonAPI
 
 def main():
     parser = argparse.ArgumentParser(description="Run the Daemon API with specified host and port")
-    parser.add_argument('--host', type=str, default='127.0.0.1', help='Host for the API server')
-    parser.add_argument('--port', type=int, default=8000, help='Port for the API server')
+    parser.add_argument('--host', type=str, default='0.0.0.0', help='Host for the API server')
+    parser.add_argument('--port', type=int, default=8080, help='Port for the API server')
     args = parser.parse_args()
 
     logging.warning("Sense server must not be on the same server as the miner/validator.")
     time.sleep(2)
-    
+
     logging.info("[⚡️] Initializing Sense...")
 
     with open('config.json', 'r') as config_file:
