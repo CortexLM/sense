429731eaee41b6441d58d796b2aafbe73ebd3b4f
Echo <echo@cortex.foundation>
2024-01-14 20:10:22 +0000

    :art: Add logs folder & create logs from logging

--- utils/logging.py
+++ utils/logging.py
@@ -1,6 +1,29 @@
 from loguru import logger
 import sys
-# Configuration du format de log sans le nom de la fonction
+import os
+import re
 
-# Exposer l'objet logger pour être importé
+# Check if the file logs/latest.log exists
+if os.path.isfile('logs/latest.log'):
+    # Open the file in read mode
+    with open('logs/latest.log', 'r') as file:
+        # Read the first line
+        first_line = file.readline()
+        
+        # Use a regular expression to extract the date
+        match = re.search(r'(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{3})', first_line)
+        
+        # If a match is found
+        if match:
+            # Extract the date
+            date_string = match.group(1)
+            
+            # Transform the filename using the date
+            new_filename = f'logs/{date_string.replace(" ", "_").replace(":", "").replace(".", "")}.log'
+            
+            # Rename the file
+            os.rename('logs/latest.log', new_filename)
+
+logger.add("logs/latest.log", level="DEBUG", format="<green>{time:YYYY-MM-DD HH:mm:ss.SSS zz}</green> | <level>{level: <8}</level> | <yellow>Line {line: >4} ({file}):</yellow> <b>{message}</b>", colorize=False, backtrace=True, diagnose=True)
+
 logging = logger
