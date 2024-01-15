afcd10190bf13bc48ee59c14b08c9895126dfd4d
Elzorro99 <38183701+Elzorro99@users.noreply.github.com>
2024-01-15 22:45:57 +0100

    Update model.py

--- utils/model.py
+++ utils/model.py
@@ -121,7 +121,7 @@ class ModelManager:
             await self.fetch_model(model_name=model_name)
             yield {"status": "downloaded", "message": "Model downloaded"}
             yield {"status": "start_process", "message": "Starting process"}
-            tm = TurboMind(self, model_path=model_path, model_name=model_name, gpu_id="0", tb_model_type=model_type, port=self.get_random_port())
+            tm = TurboMind(self, model_path=model_path, model_name=model_name, gpu_id=n_gpus, tb_model_type=model_type, port=self.get_random_port())
             yield {"status": "wait_status", "message": "Wait for status"}
             if await tm.wait_for_tb_model_status():
                 yield {"status": "ready", "message": "Model is ready"}
