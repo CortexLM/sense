4855837cdbbd53d0766eff9ccf45d909a5c0a80a
Echo <echo@cortex.foundation>
2024-01-18 18:38:37 +0000

    fix building

--- utils/turbomind.py
+++ utils/turbomind.py
@@ -176,7 +176,7 @@ class TurboMind:
             environment = os.environ.copy()
             environment["CUDA_VISIBLE_DEVICES"] = self.gpu_id
             
-            command = f"lmdeploy convert --model_format {self.tb_model_type} --quant_path {self.base_directory}{self.model_path}model --dst_path {self.base_directory}{self.model_path}workspace --model_format awq --group_size 128 --tp {count_gpu(self.gpu_id)}"
+            command = f"lmdeploy convert --model_name {self.tb_model_type} --model_path {self.base_directory}{self.model_path}model --dst_path {self.base_directory}{self.model_path}workspace --model_format awq --group_size 128 --tp {count_gpu(self.gpu_id)}"
             logging.info(f'Spawning build model for {self.model_path}')
 
             try:
